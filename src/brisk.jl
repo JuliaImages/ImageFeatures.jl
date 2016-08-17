f2ba3a4da0e38095c6777f877773cd8cd698fdd8
mronian <anchitn@gmail.com>
2016-08-17 15:15:00 +0530

    Replace DescriptorParams

--- src/brisk.jl
+++ src/brisk.jl
@@ -1,4 +1,4 @@
-type BRISK <: DescriptorParams
+type BRISK <: Params
 	threshold::Float64
 	octaves::Int
 end
@@ -7,10 +7,14 @@ function BRISK(; threshold::Float64 = 0.25, octaves::Int = 4)
 	BRISK(threshold, octaves)
 end
 
-function extract_features()
+function extract_features{T<:Gray}(img::AbstractArray{T, 2}, params::BRISK)
 
 end
 
 function create_descriptor{T<:Gray}(img::AbstractArray{T, 2}, params::BRISK)
+	features = extract_features(img, params)
+end
+
+function create_descriptor{T<:Gray}(img::AbstractArray{T, 2}, features::Array{Feature}, params::BRISK)
 
 end
