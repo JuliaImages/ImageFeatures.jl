d17d674a54b3f4b3f066e92e23e4b2456f30ae6a
Animesh <annimesh2809@gmail.com>
2017-03-20 22:39:36 +0530

    Added some more tests

--- test/houghtransform.jl
+++ test/houghtransform.jl
@@ -16,10 +16,24 @@ using ImageFeatures
                 img[i,j] = false
             end
         end
+
     #For images with diagonal line
         img = diagm([true, true ,true])
         h = hough_transform_standard(img,1,0.1,0,3.14,2,3)
         @test length(h) == 1
         @test h[1][1] == 0
+
+    #For a square image
+        img = zeros(Bool,10,10)
+        for i in 1:10
+            img[2,i] = img[i,2] = img[7,i] = img[i,9] = true
+        end
+        h = hough_transform_standard(img,1,0.01,0,3.14/2,9,10)
+        @test length(h) == 4
+        r = [h[i][1] for i in CartesianRange(size(h))]
+        @test all(r .== [2,2,7,9])
+        theta = [h[i][2] for i in CartesianRange(size(h))]
+        er = sum(map((t1,t2) -> abs(t1-t2), theta, [0, 3.14/2, 3.14/2, 0]))
+        @test er <= 0.1
     end
 end
